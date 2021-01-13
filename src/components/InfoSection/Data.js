a1505f5e9356c51149a0701bba18ea7246113fe0
jtown970 <jasonrtowner@gmail.com>
2021-01-12 18:23:54 -0700

    first push

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,53 @@
+export const homeObjOne = {
+  id: 'about',
+  lightBg: false,
+  lightText: true,
+  lightTextDesc: true,
+  topLine: 'SamPro',
+  headline: 'Crushing it is the game Sam Pro is the name',
+  description:
+    'things about me that you should have on a website ',
+  buttonLabel: 'Get Started',
+  imgStart: false,
+  img: require('../../images/svg-1.svg'),
+  alt: 'Car',
+  dark: true,
+  primary: true,
+  darkText: false
+};
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'why i like to build things',
+  headline: 'Your stuck at home lets make it nice',
+  description:
+    'I have been making homes nicer for years click below to become one of them',
+  buttonLabel: 'Learn More',
+  imgStart: true,
+  img: require('../../images/svg-2.svg'),
+  alt: 'Piggybank',
+  dark: false,
+  primary: false,
+  darkText: true
+};
+
+export const homeObjThree = {
+  id: 'signup',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'Final Sale',
+  headline: 'running out of this here',
+  description:
+    "I like this website great job jason.",
+  buttonLabel: 'Start Now',
+  imgStart: false,
+  img: require('../../images/svg-3.svg'),
+  alt: 'Papers',
+  dark: false,
+  primary: false,
+  darkText: true
+};
---
+++ src/components/Services/index.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Icon1 from '../../images/svg-1.svg';
+import Icon2 from '../../images/svg-3.svg';
+import Icon3 from '../../images/svg-5.svg';
+import {
+  ServicesContainer,
+  ServicesH1,
+  ServicesWrapper,
+  ServicesCard,
+  ServicesIcon,
+  ServicesH2,
+  ServicesP
+} from './ServicesElements';
+
+const Services = () => {
+  return (
+    <ServicesContainer id='services'>
+      <ServicesH1>Services</ServicesH1>
+      <ServicesWrapper>
+        <ServicesCard>
+          <ServicesIcon src={Icon1} />
+          <ServicesH2>Remodels</ServicesH2>
+          <ServicesP>
+            Bath, kitchen, and much more.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon2} />
+          <ServicesH2>Demo</ServicesH2>
+          <ServicesP>
+            I tear your floor and the dance floor.
+          </ServicesP>
+        </ServicesCard>
+        <ServicesCard>
+          <ServicesIcon src={Icon3} />
+          <ServicesH2>Out Doors</ServicesH2>
+          <ServicesP>
+            Handrails to outdoor living spaces. I'm great at it all
+          </ServicesP>
+        </ServicesCard>
+      </ServicesWrapper>
+    </ServicesContainer>
+  );
+};
+
+export default Services;
